271738e945f870c7c6543b6f4ef1e5c3fe6ead0d
Noah Riordan <noahriordan31@gmail.com>
2025-04-08 17:07:08 -0400

    if at first you don't succeed

--- src/NoiseBeforeDefeat.jsx
+++ src/NoiseBeforeDefeat.jsx
@@ -2945,7 +2945,7 @@ const NoiseBeforeDefeat = ({ gameMode = "standard", onGameEnd, currentUser }) =>
             className="game-board"
 
             viewBox={`
-  ${-((GRID_SIZE * 2 * CELL_SIZE) / 2) + 280} 
+  ${-((GRID_SIZE * 2 * CELL_SIZE) / 2) + 220} 
   ${-((GRID_SIZE * 2 * CELL_SIZE) / 4)} 
   ${GRID_SIZE * 2 * CELL_SIZE + 150} 
   ${GRID_SIZE * 2 * CELL_SIZE + 100}
